d2ebdc70582eb71c7c7e1670b4fe658164434987
Romeo <romeomeshack34@gmail.com>
2019-11-30 02:11:41 +0100

    User registeration

--- users/views.py
+++ users/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from users.forms import UserRegisterationForm
 from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Accounted created for {username}!')
-            return redirect('blog_home')
+            return redirect('blog:blog_home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterationForm()
         messages.warning(request, f'Hi guest! Fill in fields to join')
 
     return render(request, 'users/register.html', {'form':form})
