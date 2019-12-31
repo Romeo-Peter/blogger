3f55be2e5432b334e561796d8d5abe8d056a06b5
Romeo <romeomeshack34@gmail.com>
2019-12-31 10:07:41 +0100

    Integrated CRUD and Integrity

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='blog_home'),
-    path('about', views.about, name='blog_about'),
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('about', views.about, name='about'),
 ]
