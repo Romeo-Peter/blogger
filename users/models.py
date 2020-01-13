81c34327320c878c175c86d5d219f69432dbe39c
Romeo <romeomeshack34@gmail.com>
2020-01-13 22:36:20 +0100

    Changes

--- users/models.py
+++ users/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = models.ImageField(default='profile_pics/default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # Override save nethod. Resize images for scalability
+    def save(self, *args, **kwargs):
+
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            resize_img = (300, 300)
+            img.thumbnail(resize_img)
+            img.save(self.image.path)
+
+        # [TODO] Delete older images when updated with new images
