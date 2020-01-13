d1c763824d3d77c23495a16b68d869aa1f128742
Romeo <romeomeshack34@gmail.com>
2020-01-13 22:34:10 +0100

    User password reset

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, reverse_lazy
 
 from users import views
 from django.contrib.auth import views as auth_views
@@ -7,7 +7,31 @@ app_name = 'account'
 urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('register', views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
+    path('login', auth_views.LoginView.as_view(
+        template_name='users/accounts/login.html'), 
+        name='login'),
+
+    path('logout', auth_views.LogoutView.as_view(
+        template_name='users/accounts/logout.html'), 
+        name='logout'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/accounts/password_reset.html',
+        success_url=reverse_lazy('account:password_reset_done')
+        ),
+        name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/accounts/password_reset_done.html'), 
+    	name='password_reset_done'),
+
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetView.as_view(
+        template_name='users/accounts/password_reset.html',
+        success_url=reverse_lazy('account:password_reset_complete')
+        ), name='password_reset_confirm'),
+
+    path('password_reset_complete/', auth_views.PasswordResetView.as_view(
+        template_name='users/accounts/password_reset.html'), 
+    	name='password_reset_complete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
-from users.forms import UserRegisterationForm
+from users.forms import UserRegisterationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -13,14 +12,35 @@ def register(request):
             messages.success(request, f'Welcome {username}! You are now able to login.')
             return redirect('account:login')
         else:
-            messages.warning(request, f'Sorry! Login invalid. Register again')
+            messages.warning(request, 'Oops! Please try again')
             return redirect('account:register')
     else:
         messages.success(request, f'Hi! Fill in fields to join')
         form = UserRegisterationForm()
 
-    return render(request, 'users/register.html', {'form':form})
+    return render(request, 'users/accounts/register.html', {'form':form})
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            username = u_form.cleaned_data.get('username')
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Hello {username}, you're  profile has been update.")
+            return redirect('account:profile')
+        else:
+            messages.success(request, f'Oops! Something went wrong. Please try updating again')
+            return redirect('account:profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/accounts/profile.html', context)
